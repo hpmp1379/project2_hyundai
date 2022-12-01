05747eb3abf95079ee239a16def9ac90b2e72bbe
HPs <88412085+hpmp1379@users.noreply.github.com>
2022-12-01 11:32:05 +0900

    'update'

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(".bannerEvent section .carousel").slick({
+  infinite: true,
+  slidesToShow: 1,
+  arrows: false,
+  slidesToScroll: 1,
+  centerPadding: "60px",
+});
+
+$(".bannerSns .banner_scroll").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
