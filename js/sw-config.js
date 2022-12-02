97b3a43ec874f4c60380e950f0bd9da6067fc53d
HPs <88412085+hpmp1379@users.noreply.github.com>
2022-12-02 16:30:20 +0900

    'pwa'

---
+++ js/sw-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: ["css/*.css", "*.html", "img/**.*", "js/*.js"],
+  stripPrefix: "app/",
+  runtimeCaching: [
+    {
+      urlPattern: /this\\.is\\.a\\.regex/,
+      handler: "networkFirst",
+    },
+  ],
+};
