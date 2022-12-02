bc23e1ff796cfc40611d0e4ac76214e098e5f866
HPs <88412085+hpmp1379@users.noreply.github.com>
2022-12-02 17:15:14 +0900

    'fixpwa

--- js/sw-config.js
+++ js/sw-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  staticFileGlobs: ["*.html", "css/*.css", "images/**.*", "js/*.js"],
+  staticFileGlobs: ["*.html", "css/*.css", "img/**.*", "js/*.js"],
   stripPrefix: "app/",
   runtimeCaching: [
     {
